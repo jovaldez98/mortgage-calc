d158dd07699c44ad6448e77782cd3cf4ba5c376f
Joceliyn <jocevaldez98@gmail.com>
2023-09-27 20:56:54 -0700

    added components, created react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import components
+import Nav from './Components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
